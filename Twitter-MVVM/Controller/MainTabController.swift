ef3362782f7f04ec1f96a07505e2bb770feaf61c
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2022-01-11 18:42:28 +0100

    Initial commit.

---
+++ Twitter-MVVM/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  Twitter-MVVM
+//
+//  Created by Justyna Kowalkowska on 11/01/2022.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+
+}
